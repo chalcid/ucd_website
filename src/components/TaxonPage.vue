32cab0389d8493aca80ae51052fa7d5e3a4e2d0f
rogerburks <17787203+rogerburks@users.noreply.github.com>
2024-07-09 19:05:21 -0700

    Fixes to consistently parentheses and asserted distribution for combinations

--- src/components/TaxonPage.vue
+++ src/components/TaxonPage.vue
@@ -3,7 +3,7 @@
     <span 
       v-for="(breadcrumb, index) in reversedBreadcrumbs"
         :key="breadcrumb.id"
-        :class="{ italicizeBreadcrumb: breadcrumb.rank_string === 'NomenclaturalRank::Iczn::GenusGroup::Genus' || breadcrumb.rank_string === 'NomenclaturalRank::Iczn::GenusGroup::Subgenus' || breadcrumb.rank_string === 'NomenclaturalRank::Iczn::SpeciesGroup::Species' || breadcrumb.rank_string === 'NomenclaturalRank::Icn::GenusGroup::Genus' || breadcrumb.rank_string === 'NomenclaturalRank::Icn::SpeciesAndInfraspeciesGroup::Species' }">
+        :class="{ italicizeBreadcrumb: breadcrumb.rank_string === 'NomenclaturalRank::Iczn::GenusGroup::Genus' || breadcrumb.rank_string === 'NomenclaturalRank::Iczn::GenusGroup::Subgenus' || breadcrumb.rank_string === 'NomenclaturalRank::Iczn::SpeciesGroup::Species' || breadcrumb.rank_string === 'NomenclaturalRank::Icn::GenusGroup::Genus' || (breadcrumb.rank_string === 'NomenclaturalRank::Icn::SpeciesAndInfraspeciesGroup::Species' ) }">
         <router-link :to="{ name: 'TaxonPage', query: { taxonID: breadcrumb.id }}" v-if="breadcrumb.rank_string === 'NomenclaturalRank::Iczn::FamilyGroup::Family' || breadcrumb.rank_string === 'NomenclaturalRank::Iczn::FamilyGroup::Subfamily' || breadcrumb.rank_string === 'NomenclaturalRank::Iczn::FamilyGroup::Tribe' || breadcrumb.rank_string === 'NomenclaturalRank::Iczn::FamilyGroup::Subtribe' || breadcrumb.rank_string === 'NomenclaturalRank::Iczn::GenusGroup::Genus' || breadcrumb.rank_string === 'NomenclaturalRank::Iczn::GenusGroup::Subgenus' || breadcrumb.rank_string === 'NomenclaturalRank::Iczn::SpeciesGroup::Species' || breadcrumb.rank_string === 'NomenclaturalRank::Icn::SpeciesAndInfraspeciesGroup::Species' || breadcrumb.rank_string === 'NomenclaturalRank::Icn::GenusGroup::Genus'">
           {{ breadcrumb.name }}
         </router-link>
@@ -21,11 +21,11 @@
       </h3>
       <h3 v-else-if="italicized && taxonViewed[0] && taxonViewed[0].cached_is_valid===false && validified">
         <span v-if="taxonViewed[0].cached_html" v-html="cachedNameString"></span>
-        <span v-else v-html="validified[0].cached_html + taxonViewed[0].cached_author_year"></span>
+        <span v-else v-html="validified[0].cached_html + '(' + taxonViewed[0].cached_author_year + ')'"></span>
       </h3>
       <h3 v-else-if="italicized && taxonViewed[0] && taxonViewed[0].cached_is_valid===false">
         <span v-if="taxonViewed[0].cached_html" v-html="cachedNameString"></span>
-        <span v-else v-html="validified[0].cached_html + taxonViewed[0].cached_author_year"></span>
+        <span v-else v-html="validified[0].cached_html + '(' + taxonViewed[0].cached_author_year + ')'"></span>
       </h3>
       <h3 v-else-if="taxonViewed[0] && taxonViewed[0].cached_is_valid===true">{{ taxonViewed[0].cached }} {{ taxonViewed[0].cached_author_year }}</h3>
       <h3 v-else-if="taxonViewed[0] && taxonViewed[0].cached_is_valid===false" v-html="cachedNameString"></h3>
@@ -59,7 +59,7 @@
               <ul v-if="synonymArray" id="results-list-span">
                 <li id="results-list-item" v-for="tag in synonymArray.timeline" :key="tag" v-html="tag.label"></li>
               </ul>
-              <div class="indent" v-show="rankString==='NomenclaturalRank::Iczn::SpeciesGroup::Species' && concatenatedTypeInfo != 'Type information: '">{{ concatenatedTypeInfo }}</div>
+              <div class="indent" v-show="rankString==='NomenclaturalRank::Iczn::SpeciesGroup::Species' || isCombination === true">{{ concatenatedTypeInfo }}</div>
               <div class="indent" v-show="rankString==='NomenclaturalRank::Iczn::GenusGroup::Genus' || rankString==='NomenclaturalRank::Iczn::GenusGroup::Subgenus'">Type species: <router-link :to="{ name: 'TaxonPage', query: { taxonID: typeID }}" v-if="typeID"> <span v-html="concatenatedTypeInfo"></span></router-link></div>
               <div class="indent" v-show="rankString==='NomenclaturalRank::Iczn::FamilyGroup::Family' || rankString==='NomenclaturalRank::Iczn::FamilyGroup::Subfamily' || rankString==='NomenclaturalRank::Iczn::FamilyGroup::Tribe' || rankString==='NomenclaturalRank::Iczn::FamilyGroup::Subtribe'">Type genus: <router-link :to="{ name: 'TaxonPage', query: { taxonID: typeID }}" v-if="typeID"> <span v-html="concatenatedTypeInfo"></span></router-link></div>
             </div>
@@ -133,6 +133,7 @@ h3{
       const otu = ref('');
       const cachedNameString = ref('');
       const validTaxonID = ref('');
+      const oldTaxonID =  ref('');
       const gettingType = ref(false);
       
       const nomenclaturalReferencesResults = computed(() => {
@@ -174,8 +175,12 @@ h3{
         
         if (taxonViewed.value && taxonViewed.value[0] && taxonViewed.value[0].cached) {
           document.title = "UCD: " + taxonViewed.value[0].cached;
-          if(taxonViewed.value[0].cached_html && taxonViewed.value[0].cached_author_year)
+          if(taxonViewed.value[0].cached_html && taxonViewed.value[0].cached_author_year && state.isCombination===false) {
             cachedNameString.value = taxonViewed.value[0].cached_html + " " + taxonViewed.value[0].cached_author_year;
+          }
+          else {
+            cachedNameString.value = taxonViewed.value[0].cached_html + " (" + taxonViewed.value[0].cached_author_year + ")";
+          };
         };
         
         if(nomenclaturalReferencesResults){
@@ -194,8 +199,15 @@ h3{
         
           if(await taxonViewed.value[0].type === 'Combination') {
             state.isCombination = true
+            oldTaxonID.value = taxonViewed.value[0].id;
             validTaxonID.value = taxonViewed.value[0].cached_valid_taxon_name_id;
             taxonID.value = taxonViewed.value[0].cached_valid_taxon_name_id;
+            const otuGet = api.get(`/taxon_names/${validTaxonID.value}`,
+              {params: {
+                extend: ["otus"],
+                project_token: import.meta.env.VITE_APP_PROJECT_TOKEN
+            }})
+            state.validified = await otuGet;
           }
           else {
             rankString.value = taxonViewed.value[0].rank_string;
@@ -289,7 +301,7 @@ h3{
       };
       
       const typeInfo = async () => {
-        if(rankString.value === "NomenclaturalRank::Iczn::SpeciesGroup::Species"){
+        if(rankString.value === "NomenclaturalRank::Iczn::SpeciesGroup::Species" || state.isCombination === true){
           const response = await api.get(`/data_attributes`,
             {params: {
               attribute_subject_id: taxonID.value,
@@ -298,7 +310,7 @@ h3{
           const dataAttributesResults = response.data;
           jsonToDownload.value["Additional data"] = await dataAttributesResults;
           
-          const extractedValues = dataAttributesResults
+          const extractedValues = await dataAttributesResults
             .filter(item => ['Species:PrimType', 'Species:TypeSex', 'Coll:Depository'].includes(item.predicate_name))
             .map(item => item.value === 'LT' ? 'Lectotype' : item.value === 'HT' ? 'Holotype' : item.value === 'ST' ? 'Syntypes' : item.value === 'NT' ? 'Neotype' : item.value === 'F' ? 'Female' : item.value === 'M' ? 'Male' : item.value, controlled_vocabulary_term_id => controlled_vocabulary_term_id);
           concatenatedTypeInfo.value = "Type information: " + extractedValues.join(", ");
@@ -447,7 +459,8 @@ h3{
         familyName,
         otu,
         cachedNameString,
-        validTaxonID
+        validTaxonID,
+        oldTaxonID
       };
     }
   }
